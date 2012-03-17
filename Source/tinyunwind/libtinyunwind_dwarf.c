616aafabd4369ea885464bce17a4c7212703ae2e
gwynne@darkrainfall.org <gwynne@darkrainfall.org@74fe88e9-a195-4aad-b98c-1ad692fed28e>
2012-03-17 07:15:38 +0000

    Add CFA opcode parser. Still missing: DWARF expressions parser (necessary?) and interpreting the output state.
    git-svn-id: https://clients.plausible.coop/svn/plcrashreporter/trunk@438 74fe88e9-a195-4aad-b98c-1ad692fed28e

--- Source/tinyunwind/libtinyunwind_dwarf.c
+++ Source/tinyunwind/libtinyunwind_dwarf.c
@@ -33,6 +33,12 @@
 #import <stdio.h>
 #import <sys/param.h>
 
+#define TINYUNW_DEBUG(msg, args...) {\
+    char output[256];\
+    snprintf(output, sizeof(output), "[tinyunwind] " msg "\n", ## args); \
+    write(STDERR_FILENO, output, strlen(output));\
+}
+
 int			tinyunw_try_step_dwarf(tinyunw_real_cursor_t *cursor)
 {
 #if __x86_64__
@@ -47,12 +53,14 @@ int			tinyunw_try_step_dwarf(tinyunw_real_cursor_t *cursor)
     
     if (fde) {
         /* Try using it here! */
-#define PLCF_DEBUG(msg, args...) {\
-    char output[256];\
-    snprintf(output, sizeof(output), "[PLCrashReport] " msg "\n", ## args); \
-    write(STDERR_FILENO, output, strlen(output));\
-}
-PLCF_DEBUG("Found an FDE 0x%p in image 0x%lx for ip 0x%llx with range 0x%lx-0x%lx", fde, image->header, cursor->current_context.__rip, fde->initialLocation, fde->finalLocation);
+        tinyunw_dwarf_cfa_state_t state;
+        
+        tinyunw_dwarf_run_cfa_for_fde(fde, cursor->current_context.__rip, &state);
+        TINYUNW_DEBUG("Generated state:");
+        TINYUNW_DEBUG("\tCFA register: %llu + 0x%llx exp 0x%ld", state.cfaRegister, state.cfaOffset, state.cfaExpression);
+        for (int i = 0; i < TINYUNW_SAVED_REGISTER_COUNT; ++i) {
+            TINYUNW_DEBUG("\tRegister %d: 0x%llx @ %d", i, state.savedRegisters[i].value, state.savedRegisters[i].saveLocation);
+        }
     }
 #endif    
     return TINYUNW_ENOFRAME;
@@ -74,6 +82,43 @@ enum {
     DW_EH_PE_pcrel = 0x10, /* rel. to addr. of encoded value */
 };
 
+enum
+{
+    DWARF_CFA_OPCODE_MASK = 0xc0,
+    DWARF_CFA_OPERAND_MASK = 0x3f,
+    DW_CFA_advance_loc = 0x40,
+    DW_CFA_offset = 0x80,
+    DW_CFA_restore = 0xc0,
+    DW_CFA_nop = 0x00,
+    DW_CFA_set_loc = 0x01,
+    DW_CFA_advance_loc1 = 0x02,
+    DW_CFA_advance_loc2 = 0x03,
+    DW_CFA_advance_loc4 = 0x04,
+    DW_CFA_offset_extended = 0x05,
+    DW_CFA_restore_extended = 0x06,
+    DW_CFA_undefined = 0x07,
+    DW_CFA_same_value = 0x08,
+    DW_CFA_register = 0x09,
+    DW_CFA_remember_state = 0x0a,
+    DW_CFA_restore_state = 0x0b,
+    DW_CFA_def_cfa = 0x0c,
+    DW_CFA_def_cfa_register = 0x0d,
+    DW_CFA_def_cfa_offset = 0x0e,
+    DW_CFA_def_cfa_expression = 0x0f,
+    DW_CFA_expression = 0x10,
+    DW_CFA_offset_extended_sf = 0x11,
+    DW_CFA_def_cfa_sf = 0x12,
+    DW_CFA_def_cfa_offset_sf = 0x13,
+    DW_CFA_val_offset = 0x14,
+    DW_CFA_val_offset_sf = 0x15,
+    DW_CFA_val_expression = 0x16,
+    DW_CFA_lo_user = 0x1c,
+    DW_CFA_GNU_window_save = 0x2d,
+    DW_CFA_GNU_args_size = 0x2e,
+    DW_CFA_GNU_negative_offset_extended = 0x2f,
+    DW_CFA_hi_user = 0x3c,
+};
+
 /* Return from function with error if there aren't enough bytes left. */
 #define ENSURE_BYTES(l, ml, b)  do { if ((ml) - (*l) < (b)) { return TINYUNW_EUNSPEC; } } while (0);
 #define tinyunw_dwarf_read_u8(l, ml) ({ uint8_t v = 0; ENSURE_BYTES(l, ml, sizeof(uint8_t)); v = *(uint8_t *)(*l); (*l) += sizeof(uint8_t); v; })
@@ -84,8 +129,10 @@ enum {
 #define tinyunw_dwarf_read_s32(l, ml) ({ int32_t v = 0; ENSURE_BYTES(l, ml, sizeof(int32_t)); v = *(int32_t *)(*l); (*l) += sizeof(int32_t); v; })
 #define tinyunw_dwarf_read_u64(l, ml) ({ uint64_t v = 0; ENSURE_BYTES(l, ml, sizeof(uint64_t)); v = *(uint64_t *)(*l); (*l) += sizeof(uint64_t); v; })
 #define tinyunw_dwarf_read_s64(l, ml) ({ int64_t v = 0; ENSURE_BYTES(l, ml, sizeof(int64_t)); v = *(int64_t *)(*l); (*l) += sizeof(int64_t); v; })
+#define tinyunw_dwarf_read_u(l, ml, n) ((n == 1 ? tinyunw_dwarf_read_u8(l, ml) : (n == 2 ? tinyunw_dwarf_read_u16(l, ml) : tinyunw_dwarf_read_u32(l, ml))))
 #define tinyunw_dwarf_read_uleb128(l, ml) ({ uint64_t v = 0; if (_tinyunw_dwarf_read_uleb128((l), (ml), &v)) { return TINYUNW_EUNSPEC; } v; })
 #define tinyunw_dwarf_read_sleb128(l, ml) ({ int64_t v = 0; if (_tinyunw_dwarf_read_sleb128((l), (ml), &v)) { return TINYUNW_EUNSPEC; } v; })
+#define tinyunw_dwarf_read_leb128(l, ml, s) (s ? tinyunw_dwarf_read_sleb128(l, ml) : tinyunw_dwarf_read_uleb128(l, ml))
 #define tinyunw_dwarf_read_pointer(l, ml) (sizeof(uintptr_t) == sizeof(uint64_t) ? tinyunw_dwarf_read_u64(l, ml) : tinyunw_dwarf_read_u32(l, ml))
 #define tinyunw_dwarf_read_encoded_pointer(l, ml, e) ({ uintptr_t v = 0; if (_tinyunw_dwarf_read_encoded_pointer((l), (ml), (e), &v)) { return TINYUNW_EUNSPEC; } v; })
 
@@ -444,4 +491,171 @@ tinyunw_dwarf_fde_t *tinyunw_dwarf_fde_list_search(tinyunw_dwarf_fde_list_t *lis
             return &list->fdes[i];
     }
     return NULL;
+}
+
+int tinyunw_dwarf_run_cfa_for_fde(tinyunw_dwarf_fde_t *fde, uintptr_t ip, tinyunw_dwarf_cfa_state_t *results)
+{
+    tinyunw_dwarf_cfa_state_t stateStack[16];
+    int nstack = 0, result = 0;
+    
+    memset(stateStack, 0, sizeof(stateStack));
+    result = tinyunw_dwarf_run_cfa_program(fde->cie, fde->cie->initialInstructionsStart, fde->cie->cieEnd, (uintptr_t)-1, stateStack, 16, &nstack);
+    if (result == TINYUNW_ESUCCESS)
+        result = tinyunw_dwarf_run_cfa_program(fde->cie, fde->instructionsStart, fde->fdeEnd, fde->initialLocation - ip, stateStack, 16, &nstack);
+    if (result == TINYUNW_ESUCCESS)
+        *results = stateStack[nstack];
+    return result;
+}
+
+const char *tinyunw_dwarf_opname(tinyunw_word_t opcode) {
+    switch (opcode) {
+        #define OP(n)   case n: return #n
+        OP(DW_CFA_nop);                 OP(DW_CFA_set_loc);             OP(DW_CFA_advance_loc1);    OP(DW_CFA_advance_loc2);
+        OP(DW_CFA_offset);              OP(DW_CFA_restore);             OP(DW_CFA_advance_loc4);    OP(DW_CFA_offset_extended);
+        OP(DW_CFA_restore_extended);    OP(DW_CFA_undefined);           OP(DW_CFA_same_value);      OP(DW_CFA_register);
+        OP(DW_CFA_remember_state);      OP(DW_CFA_restore_state);       OP(DW_CFA_def_cfa);         OP(DW_CFA_def_cfa_register);
+        OP(DW_CFA_def_cfa_offset);      OP(DW_CFA_def_cfa_expression);  OP(DW_CFA_expression);      OP(DW_CFA_offset_extended_sf);
+        OP(DW_CFA_def_cfa_sf);          OP(DW_CFA_def_cfa_offset_sf);   OP(DW_CFA_val_offset);      OP(DW_CFA_val_offset_sf);
+        OP(DW_CFA_val_expression);      OP(DW_CFA_lo_user);             OP(DW_CFA_GNU_window_save); OP(DW_CFA_GNU_args_size);
+        OP(DW_CFA_hi_user);             OP(DW_CFA_GNU_negative_offset_extended);
+        #undef OP
+    }
+    return "unknown";
+}
+
+/**
+  * @internal
+  * The results are placed on the top of the stack (stack[nstack]).
+  */
+#define TINYUNW_OP_DEBUG(msg, args...) TINYUNW_DEBUG("%s " msg, tinyunw_dwarf_opname(opcode), ## args)
+
+int tinyunw_dwarf_run_cfa_program(tinyunw_dwarf_cie_t *cie, uintptr_t instrStart, uintptr_t instrEnd, uintptr_t ipLimit,
+                                  tinyunw_dwarf_cfa_state_t *stack, int maxstack, int *nstack)
+{
+    uintptr_t ipCurrent = 0;
+    tinyunw_dwarf_cfa_state_t initialState = stack[*nstack];
+
+    TINYUNW_DEBUG("Starting CFA program at 0x%lx IP 0x%lx", instrStart, ipCurrent);
+    
+    while (instrStart < instrEnd && ipCurrent < ipLimit) {
+        uint8_t opcode = tinyunw_dwarf_read_u8(&instrStart, instrEnd);
+        tinyunw_word_t operand1 = 0, operand2 = 0;
+        
+        if ((opcode & DWARF_CFA_OPCODE_MASK) != 0) {
+            operand1 = opcode & DWARF_CFA_OPERAND_MASK;
+            opcode &= DWARF_CFA_OPCODE_MASK;
+        }
+        switch (opcode) {
+            case DW_CFA_nop:
+                TINYUNW_OP_DEBUG("");
+                break;
+            case DW_CFA_set_loc:
+                ipCurrent = tinyunw_dwarf_read_encoded_pointer(&instrStart, instrEnd, cie->pointerEncoding);
+                TINYUNW_OP_DEBUG("0x%lx", ipCurrent);
+                break;
+            case DW_CFA_advance_loc:
+            case DW_CFA_advance_loc1:
+            case DW_CFA_advance_loc2:
+            case DW_CFA_advance_loc4:
+                if (opcode != DW_CFA_advance_loc)
+                    operand1 = tinyunw_dwarf_read_u(&instrStart, instrEnd, 1 << (opcode - 2));
+                ipCurrent += operand1 * cie->codeAlignmentFactor;
+				TINYUNW_OP_DEBUG("0x%llx ip = 0x%lx", operand1, ipCurrent);
+                break;
+            case DW_CFA_offset:
+            case DW_CFA_offset_extended:
+            case DW_CFA_offset_extended_sf:
+            case DW_CFA_val_offset:
+            case DW_CFA_val_offset_sf:
+                if (opcode != DW_CFA_offset)
+                    operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                operand2 = tinyunw_dwarf_read_leb128(&instrStart, instrEnd,
+                    opcode == DW_CFA_offset_extended_sf || opcode == DW_CFA_val_offset_sf) * cie->dataAlignmentFactor;
+                stack[*nstack].savedRegisters[operand1].saveLocation = (opcode == DW_CFA_val_offset || opcode == DW_CFA_val_offset_sf ?
+                    TINYUNW_REG_OFFSET_CFA : TINYUNW_REG_CFA);
+                stack[*nstack].savedRegisters[operand1].value = operand2;
+                TINYUNW_OP_DEBUG("%llu = 0x%llx", operand1, operand2);
+                break;
+            case DW_CFA_restore:
+            case DW_CFA_restore_extended:
+                if (opcode != DW_CFA_restore)
+                    operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                stack[*nstack].savedRegisters[operand1] = initialState.savedRegisters[operand1];
+                TINYUNW_OP_DEBUG("%llu = 0x%llx @ %u", operand1, initialState.savedRegisters[operand1].value, initialState.savedRegisters[operand1].saveLocation);
+                break;
+            case DW_CFA_undefined:
+            case DW_CFA_same_value:
+                /* Per Apple's implementation, same_value is modeled identically to undefined. */
+                operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                stack[*nstack].savedRegisters[operand1].saveLocation = TINYUNW_REG_UNUSED;
+                TINYUNW_OP_DEBUG("%llu", operand1);
+                break;
+            case DW_CFA_register:
+                operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                operand2 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                stack[*nstack].savedRegisters[operand1].saveLocation = TINYUNW_REG_REG;
+                stack[*nstack].savedRegisters[operand1].value = stack[*nstack].savedRegisters[operand2].value;
+                TINYUNW_OP_DEBUG("%llu = %llu", operand1, operand2);
+                break;
+            case DW_CFA_remember_state:
+                if (++*nstack >= maxstack)
+                    return TINYUNW_ENOMEM;
+                stack[*nstack] = stack[*nstack - 1];
+                TINYUNW_OP_DEBUG("");
+                break;
+            case DW_CFA_restore_state:
+                if (--*nstack < 0)
+                    return TINYUNW_EBADFRAME;
+                stack[*nstack] = stack[*nstack + 1];
+                TINYUNW_OP_DEBUG("");
+                break;
+            case DW_CFA_def_cfa:
+            case DW_CFA_def_cfa_sf:
+                stack[*nstack].cfaRegister = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                stack[*nstack].cfaOffset = tinyunw_dwarf_read_leb128(&instrStart, instrEnd, opcode == DW_CFA_def_cfa_sf) *
+                                            (opcode == DW_CFA_def_cfa_sf ? cie->dataAlignmentFactor : 1);
+                TINYUNW_OP_DEBUG("%llu + %lld", stack[*nstack].cfaRegister, stack[*nstack].cfaOffset);
+                break;
+            case DW_CFA_def_cfa_register:
+                stack[*nstack].cfaRegister = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                TINYUNW_OP_DEBUG("%llu", stack[*nstack].cfaRegister);
+                break;
+            case DW_CFA_def_cfa_offset:
+            case DW_CFA_def_cfa_offset_sf:
+                stack[*nstack].cfaOffset = tinyunw_dwarf_read_leb128(&instrStart, instrEnd, opcode == DW_CFA_def_cfa_sf) *
+                                            (opcode == DW_CFA_def_cfa_sf ? cie->dataAlignmentFactor : 1);
+                TINYUNW_OP_DEBUG("%lld", stack[*nstack].cfaOffset);
+                break;
+            case DW_CFA_def_cfa_expression:
+                stack[*nstack].cfaRegister = 0;
+                stack[*nstack].cfaExpression = instrStart;
+                operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                instrStart += operand1;
+                TINYUNW_OP_DEBUG("0x%lx-0x%lx", stack[*nstack].cfaExpression, instrStart);
+                break;
+            case DW_CFA_expression:
+            case DW_CFA_val_expression:
+                operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                stack[*nstack].savedRegisters[operand1].saveLocation = (opcode == DW_CFA_expression ? TINYUNW_REG_ATEXP : TINYUNW_REG_ISEXP);
+                stack[*nstack].savedRegisters[operand1].value = instrStart;
+                operand2 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                instrStart += operand2;
+                TINYUNW_OP_DEBUG("0x%llx-0x%lx", stack[*nstack].savedRegisters[operand1].value, instrStart);
+                break;
+            case DW_CFA_GNU_args_size: /* We don't currently use this value. */
+                operand1 = tinyunw_dwarf_read_uleb128(&instrStart, instrEnd);
+                TINYUNW_OP_DEBUG("%llu", operand1);
+                break;
+                TINYUNW_OP_DEBUG("");
+                return TINYUNW_EINVAL;
+            case DW_CFA_GNU_negative_offset_extended: /* Per comments in GCC, this opcode is only used by old PPC code. */
+            case DW_CFA_GNU_window_save: /* This is a SPARC-specific opcode. */
+            case DW_CFA_lo_user: /* Unused. */
+            case DW_CFA_hi_user:
+            default:
+                TINYUNW_OP_DEBUG("");
+                return TINYUNW_EINVAL;
+        }
+    }
+    return TINYUNW_ESUCCESS;
 }
--- Source/tinyunwind/libtinyunwind_dwarf.h
+++ Source/tinyunwind/libtinyunwind_dwarf.h
@@ -46,54 +46,54 @@
   * is upconverted to 64-bit in parsed structures.
   */
 struct tinyunw_dwarf_cie_t {
-	/** The CIE's raw offset within the debug information section. */
-	uintptr_t cieLocation;
-	
-	/** The CIE's starting and ending offset within the debug info section, minus length. */
-	uintptr_t cieStart, cieEnd;
-	
-	/** The CIE's length, not counting the length field. This is the parsed
-	  * length value, not the raw length bytes. */
-	size_t length;
-	
-	/** The CIE ID. Not particularly useful, included for completeness. */
-	uint64_t cieID;
-	
-	/** The CIE version. Either 1 (GCC) or 3 (DWARF 2 spec). */
-	uint8_t version;
-	
-	/** Flag whether FDEs based on this CIE have augmentation data. */
-	bool hasAugmentationData;
-	
-	/** Augmentation data size, if hasAugmentationData is true. */
-	uint64_t augmentationDataSize;
-	
-	/** Personality routine pointer, if any. */
-	uintptr_t personalityRoutine;
-	
-	/** Language-Specific Data Area encoding. */
-	uint8_t lsdaEncoding;
-	
-	/** Pointer encoding format for FDEs. */
-	uint8_t pointerEncoding;
-	
-	/** Signal frame flag. */
-	bool isSignalFrame;
-	
-	/** Code alignment factor. Factored out of advance location instructions. */
-	uint64_t codeAlignmentFactor;
-	
-	/** Data alignment factor. Factored out of offset instructions. */
-	int64_t dataAlignmentFactor;
-	
-	/** Return address register column. */
-	uint8_t returnAddressColumn;
-	
-	/** The raw offset in the debug info section to the initial instructions.
-	  * This is not a set of parsed instructions because that is only done at
-	  * DWARF step time. This structure is meant to reduce memory allocation,
-	  * not encapsulate all available information. */
-	uintptr_t initialInstructionsStart;
+    /** The CIE's raw offset within the debug information section. */
+    uintptr_t cieLocation;
+    
+    /** The CIE's starting and ending offset within the debug info section, minus length. */
+    uintptr_t cieStart, cieEnd;
+    
+    /** The CIE's length, not counting the length field. This is the parsed
+      * length value, not the raw length bytes. */
+    size_t length;
+    
+    /** The CIE ID. Not particularly useful, included for completeness. */
+    uint64_t cieID;
+    
+    /** The CIE version. Either 1 (GCC) or 3 (DWARF 2 spec). */
+    uint8_t version;
+    
+    /** Flag whether FDEs based on this CIE have augmentation data. */
+    bool hasAugmentationData;
+    
+    /** Augmentation data size, if hasAugmentationData is true. */
+    uint64_t augmentationDataSize;
+    
+    /** Personality routine pointer, if any. */
+    uintptr_t personalityRoutine;
+    
+    /** Language-Specific Data Area encoding. */
+    uint8_t lsdaEncoding;
+    
+    /** Pointer encoding format for FDEs. */
+    uint8_t pointerEncoding;
+    
+    /** Signal frame flag. */
+    bool isSignalFrame;
+    
+    /** Code alignment factor. Factored out of advance location instructions. */
+    uint64_t codeAlignmentFactor;
+    
+    /** Data alignment factor. Factored out of offset instructions. */
+    int64_t dataAlignmentFactor;
+    
+    /** Return address register column. */
+    uint8_t returnAddressColumn;
+    
+    /** The raw offset in the debug info section to the initial instructions.
+      * This is not a set of parsed instructions because that is only done at
+      * DWARF step time. This structure is meant to reduce memory allocation,
+      * not encapsulate all available information. */
+    uintptr_t initialInstructionsStart;
 };
 typedef struct tinyunw_dwarf_cie_t tinyunw_dwarf_cie_t;
 
@@ -101,31 +101,31 @@ typedef struct tinyunw_dwarf_cie_t tinyunw_dwarf_cie_t;
   * A parsed Frame Descriptor Entry.
   */
 struct tinyunw_dwarf_fde_t {
-	/** The FDE's raw offset within the debug information section. */
-	uintptr_t fdeLocation;
-	
-	/** The FDE's starting and ending offset within the debug info section, minus length. */
-	uintptr_t fdeStart, fdeEnd;
-	
-	/** The FDE's length. Same notation as CIE length. */
-	size_t length;
-	
-	/** The CIE associated with this FDE. */
-	tinyunw_dwarf_cie_t *cie;
-	
-	/** The absolute IP of the first instruction this FDE refers to. */
-	uintptr_t initialLocation;
-	
-	/** The absolute IP of the last instruction this FDE refers to. */
-	uintptr_t finalLocation;
-	
-	/** The raw offset in the debug info section to this FDE's instructions.
-	  * See notation for tinyunw_dwarf_cie_t.initialInstructionsStart.
-	  */
-	uintptr_t instructionsStart;
-	
-	/** The offset in the debug info section to the FDE's LSDA. */
-	uintptr_t lsdaStart;
+    /** The FDE's raw offset within the debug information section. */
+    uintptr_t fdeLocation;
+    
+    /** The FDE's starting and ending offset within the debug info section, minus length. */
+    uintptr_t fdeStart, fdeEnd;
+    
+    /** The FDE's length. Same notation as CIE length. */
+    size_t length;
+    
+    /** The CIE associated with this FDE. */
+    tinyunw_dwarf_cie_t *cie;
+    
+    /** The absolute IP of the first instruction this FDE refers to. */
+    uintptr_t initialLocation;
+    
+    /** The absolute IP of the last instruction this FDE refers to. */
+    uintptr_t finalLocation;
+    
+    /** The raw offset in the debug info section to this FDE's instructions.
+      * See notation for tinyunw_dwarf_cie_t.initialInstructionsStart.
+      */
+    uintptr_t instructionsStart;
+    
+    /** The offset in the debug info section to the FDE's LSDA. */
+    uintptr_t lsdaStart;
 };
 typedef struct tinyunw_dwarf_fde_t tinyunw_dwarf_fde_t;
 
@@ -133,28 +133,69 @@ typedef struct tinyunw_dwarf_fde_t tinyunw_dwarf_fde_t;
   * A list of parsed FDEs, including associated CIE lookup table.
   */
 struct tinyunw_dwarf_fde_list_t {
-	/** The number of CIEs in thist list. */
-	int ncies;
-	
-	/** The number of allocated spaces for CIEs in this list. Only used during parsing. */
-	int cieCapacity;
-	
-	/** The data storage area for CIEs. */
-	tinyunw_dwarf_cie_t *cies;
-	
-	/** The number of FDEs in this list. */
-	int nfdes;
-	
-	/** The number of allocated spaces for FDEs in this list. Only used during parsing. */
-	int fdeCapacity;
-	
-	/** The data storage area for FDEs. */
-	tinyunw_dwarf_fde_t *fdes;
-	
-	/** The async-safe FDE list. */
-	tinyunw_async_list_t fdeList;
+    /** The number of CIEs in thist list. */
+    int ncies;
+    
+    /** The number of allocated spaces for CIEs in this list. Only used during parsing. */
+    int cieCapacity;
+    
+    /** The data storage area for CIEs. */
+    tinyunw_dwarf_cie_t *cies;
+    
+    /** The number of FDEs in this list. */
+    int nfdes;
+    
+    /** The number of allocated spaces for FDEs in this list. Only used during parsing. */
+    int fdeCapacity;
+    
+    /** The data storage area for FDEs. */
+    tinyunw_dwarf_fde_t *fdes;
+    
+    /** The async-safe FDE list. */
+    tinyunw_async_list_t fdeList;
 };
 typedef struct tinyunw_dwarf_fde_list_t tinyunw_dwarf_fde_list_t;
+
+/**
+  * The number of saved registers in DWARF for x86_64.
+  *
+  * @note Apple's libunwind saves a whopping 120 registers. Memory usage much?
+  */
+enum { TINYUNW_SAVED_REGISTER_COUNT = 17 };
+
+/**
+  * The location of a register value in a CFA state.
+  */
+enum {
+    TINYUNW_REG_UNUSED = 0,
+    TINYUNW_REG_CFA,
+    TINYUNW_REG_OFFSET_CFA,
+    TINYUNW_REG_REG,
+    TINYUNW_REG_ATEXP,
+    TINYUNW_REG_ISEXP
+};
+
+/**
+  * A saved state obtained from running a DWARF CFA program.
+  */
+struct tinyunw_dwarf_cfa_state_t {
+    /** The saved registers. */
+    struct tinyunw_dwarf_saved_register_t {
+
+        /** The location from which the register value is obtained. */
+        int saveLocation;
+        
+        /** The actual register value. */
+        tinyunw_word_t value;
+    } savedRegisters[TINYUNW_SAVED_REGISTER_COUNT];
+    
+    /** The CFA register. */
+    tinyunw_word_t cfaRegister;
+    int64_t cfaOffset;
+    uintptr_t cfaExpression;
+};
+typedef struct tinyunw_dwarf_cfa_state_t tinyunw_dwarf_cfa_state_t;
+
 
 /** General parsing routines. */
 
@@ -188,3 +229,15 @@ int tinyunw_dwarf_fde_list_add_fde(tinyunw_dwarf_fde_list_t *list, tinyunw_dwarf
   * an async-signal safe list, it can not do a more efficient search.
   */
 tinyunw_dwarf_fde_t *tinyunw_dwarf_fde_list_search(tinyunw_dwarf_fde_list_t *list, uintptr_t ip);
+
+
+/** CFA program routines. */
+
+int tinyunw_dwarf_run_cfa_for_fde(tinyunw_dwarf_fde_t *fde, uintptr_t ip, tinyunw_dwarf_cfa_state_t *results);
+
+/**
+  * @internal
+  * The results are placed on the top of the stack (stack[nstack]).
+  */
+int tinyunw_dwarf_run_cfa_program(tinyunw_dwarf_cie_t *cie, uintptr_t instrStart, uintptr_t instrEnd, uintptr_t ipLimit,
+                                  tinyunw_dwarf_cfa_state_t *stack, int maxstack, int *nstack);
